6674aec5958becc0aabd4196af13e2bd65f70e3a
MouKar <49469319+MouKar@users.noreply.github.com>
2024-11-22 20:49:22 +0530

    Update test_m.py

--- app/test_m.py
+++ app/test_m.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
 import joblib
-# from sklearn.preprocessing import StandardScaler
+from app import process_upload
 from sklearn.metrics import mean_squared_error
 import streamlit as st
-model = joblib.load('model.joblib')
-def process_upload(file_csv):
+# model = joblib.load('model.joblib')
+# def process_upload(file_csv):
     
-    with st.form("my_form"):
-        uploaded_file = st.file_uploader("Choose a CSV file")
-        submitted=st.form_submit_button()
+#     with st.form("my_form"):
+#         uploaded_file = st.file_uploader("Choose a CSV file")
+#         submitted=st.form_submit_button()
 submitted=process_upload(file_csv))
 if submitted:
     # To read the CSV file as a pandas DataFrame:
